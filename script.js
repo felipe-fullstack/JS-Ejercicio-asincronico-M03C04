b4b75f453392670c3f13162e41da199174215b65
felipe-fullstack <104031049+felipe-fullstack@users.noreply.github.com>
2022-05-10 19:46:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+$(document).ready(function(){
+    $.ajax(
+        {
+            type: "GET",
+            url: "https://api.chucknorris.io/jokes/random",
+            dataType: "json",
+            success: function (data) {
+                $('#h2').text(data.value);
+            }
+        });
+})
 $('#boton').click(function () {
     $.ajax(
         {
