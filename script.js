052b97ea7de3c0309d8c8673633361ec3d7eac16
felipe-fullstack <104031049+felipe-fullstack@users.noreply.github.com>
2022-05-10 19:36:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$('#boton').click(function () {
+    $.ajax(
+        {
+            type: "GET",
+            url: "https://api.chucknorris.io/jokes/random",
+            dataType: "json",
+            success: function (data) {
+                $('#h2').text(data.value);
+            }
+        });
+});
